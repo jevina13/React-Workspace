dc007109007bcbcb7ad9b8b097fd52a517c99c67
jevin13 <jevina131999@gmail.com>
2024-01-27 14:45:28 +0530

    react first proj

---
+++ todo-app/src/components/learning-examples/LearningJavaScript.jsx
@@ -0,0 +1,23 @@
+const person = {
+  name: "Jevina Verghese",
+  address: {
+    line1: "Baker Street",
+    city: "London",
+    country: "UK",
+  },
+  profiles: ["twitter", "linkedin", "instagram"], //array in JS
+  printProfile: () => {
+    person.profiles.map(profile) => console.log(profile));
+  },
+};
+
+export default function LearningJavaScript() {
+  return (
+    <>
+      <div>{person.name}</div>
+      <div>{person.address.line1}</div>
+      <div>{person.profiles[0]}</div>
+      <div>{person.printProfile()}</div>
+    </>
+  );
+}
