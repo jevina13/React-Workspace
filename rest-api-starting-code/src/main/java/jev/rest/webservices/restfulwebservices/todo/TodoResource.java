b3925e37784c32caeef2af60053e34cc77b89a7e
jevin13 <jevina131999@gmail.com>
2024-02-06 17:04:50 +0530

    put and post mapping

--- rest-api-starting-code/src/main/java/jev/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ rest-api-starting-code/src/main/java/jev/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -6,6 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,5 +37,20 @@ public class TodoResource {
 								@PathVariable int id) {
 		todoService.deleteById(id);
 		return ResponseEntity.noContent().build();		//in delete mapping , can either choose to return succes or no contennt
+	}
+	
+	@PutMapping("/users/{username}/todos/{id}")				//used to update data/table
+	public Todo updateTodo(@PathVariable String username,
+								@PathVariable int id,@RequestBody Todo todo) {
+		todoService.updateTodo(todo);
+		return todo;		
+	}
+	
+	@PostMapping("/users/{username}/todos")			//used to create new table
+	public Todo createTodo(@PathVariable String username,
+			@RequestBody Todo todo) {
+		Todo createdTodo = todoService.addTodo(username, todo.getDescription(), 
+				todo.getTargetDate(), todo.isDone());
+		return createdTodo;		
 	}
 }
